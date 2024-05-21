5e25a1fccfffd5b6d11280d1bdf88e025889ba57
LEON <jj18601719999@163.com>
2024-05-21 22:02:53 +0800

    view data

--- my_prediction.py
+++ my_prediction.py
@@ -4,4 +4,9 @@ import pandas as pd
 train_data = pd.read_csv("data/train.csv")
 test_data = pd.read_csv("data/test.csv")
 
+print(train_data.info())
+print(test_data.info())
+
+print(train_data.describe())
+print(test_data.describe())
 # TODO
